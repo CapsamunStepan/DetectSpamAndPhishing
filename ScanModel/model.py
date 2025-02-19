385ae5a333f23e1206e52d25fd69bd33f2f8f457
Steve <stepan.capsamun@isa.utm.md>
2025-02-19 22:56:48 +0200

    new dataset

--- ScanModel/model.py
+++ ScanModel/model.py
@@ -7,18 +7,19 @@ import joblib
 
 
 class ModelTrain:
-    def __init__(self, csv_dataset, model_name):
+    def __init__(self, csv_dataset, model_name, vectorizer_name):
         self.svm_model = None
         self.csvDataset = csv_dataset
         self.modelName = model_name
         self.data = self.loadData()
         self.X_train, self.X_test, self.y_train, self.y_test = None, None, None, None
+        self.vectorizerName = vectorizer_name
         self.vectorizer = None
         self.X_train_tfidf, self.X_test_tfidf = None, None
         self.y_pred = None
 
     def loadData(self):
-        return pd.read_csv(self.csvDataset)
+        return pd.read_csv(self.csvDataset, names=["type", "text"], usecols=[0, 1], skiprows=1, header=None)
 
     def trainModel(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.data['text'],
@@ -30,6 +31,13 @@ class ModelTrain:
                                           max_df=0.9,
                                           min_df=5,
                                           max_features=5000)
+        self.X_train = self.X_train.fillna("")
+        self.X_test = self.X_test.fillna("")
+        self.y_train = self.y_train.fillna("")
+        self.y_test = self.y_test.fillna("")
+        self.y_train = self.y_train.str.lower()
+        self.y_test = self.y_test.str.lower()
+
         self.X_train_tfidf = self.vectorizer.fit_transform(self.X_train)
         self.X_test_tfidf = self.vectorizer.transform(self.X_test)
 
@@ -37,7 +45,7 @@ class ModelTrain:
         self.svm_model.fit(self.X_train_tfidf, self.y_train)
 
         joblib.dump(self.svm_model, f'{self.modelName}.pkl')
-        joblib.dump(self.vectorizer, 'tfidf_vectorizer.pkl')
+        joblib.dump(self.vectorizer, f'{self.vectorizerName}.pkl')
 
     def checkAccuracy(self):
         self.y_pred = self.svm_model.predict(self.X_test_tfidf)
@@ -48,7 +56,18 @@ class ModelTrain:
 
 
 if __name__ == '__main__':
-    train = ModelTrain('sms_spam.csv', 'spam_model')
+    # train = ModelTrain(
+    #     'sms_spam.csv',
+    #     'spam_model',
+    #     'tfidf_vectorizer'
+    # )  # accuracy 0.98
+
+    train = ModelTrain(
+        'merged_sms_spam2.csv',
+        'merged_spam_model2',
+        'tfidf_vectorizer2'
+    )  # accuracy 0.96
+
     train.trainModel()
     print(train.checkAccuracy())
     print(train.classificationReport())
